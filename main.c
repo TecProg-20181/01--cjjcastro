73ef31dc769a585cb4e5db0631b8c6fb97a1b199
cjjcastro <cjj.castro3@gmail.com>
2018-03-16 18:43:31 -0300

    Acrescentando testes as funções

--- main.c
+++ main.c
@@ -16,115 +16,15 @@ typedef struct _image {
     unsigned int h;
 } Image;
 
-
-int max(int a, int b) {
-    if (a > b)
-        return a;
-    return b;
-}
-
-int pixel_igual(Pixel p1, Pixel p2) {
-    if (p1.r == p2.r &&
-        p1.g == p2.g &&
-        p1.b == p2.b)
-        return 1;
-    return 0;
-}
-
-
-Image escala_de_cinza(Image img) {
-    /*for (unsigned int i = 0; i < img.h; ++i) {
-        for (unsigned int j = 0; j < img.w; ++j) {
-            print("%u", img.pixel[i][j][0] + img.pixel[i][j][1] + img.pixel[i][j][2]);
-        }
-    }*/
-
-    for (unsigned int i = 0; i < img.h; ++i) {
-        for (unsigned int j = 0; j < img.w; ++j) {
-            int media = img.pixel[i][j][0] +
-                        img.pixel[i][j][1] +
-                        img.pixel[i][j][2];
-            media /= 3;
-            img.pixel[i][j][0] = media;
-            img.pixel[i][j][1] = media;
-            img.pixel[i][j][2] = media;
-        }
-    }
-
-    return img;
-}
-
-void blur(unsigned int h, unsigned short int pixel[512][512][3], int T, unsigned int w) {
-    for (unsigned int i = 0; i < h; ++i) {
-        for (unsigned int j = 0; j < w; ++j) {
-            Pixel media = {0, 0, 0};
-
-            int menor_h = (h - 1 > i + T/2) ? i + T/2 : h - 1;
-            int min_w = (w - 1 > j + T/2) ? j + T/2 : w - 1;
-            for(int x = (0 > i - T/2 ? 0 : i - T/2); x <= menor_h; ++x) {
-                for(int y = (0 > j - T/2 ? 0 : j - T/2); y <= min_w; ++y) {
-                    media.r += pixel[x][y][0];
-                    media.g += pixel[x][y][1];
-                    media.b += pixel[x][y][2];
-                }
-            }
-
-            // printf("%u", media.r)
-            media.r /= T * T;
-            media.g /= T * T;
-            media.b /= T * T;
-
-            pixel[i][j][0] = media.r;
-            pixel[i][j][1] = media.g;
-            pixel[i][j][2] = media.b;
-        }
-    }
-}
-
-Image rotacionar90direita(Image img) {
-    Image rotacionada;
-
-    rotacionada.w = img.h;
-    rotacionada.h = img.w;
-
-    for (unsigned int i = 0, y = 0; i < rotacionada.h; ++i, ++y) {
-        for (int j = rotacionada.w - 1, x = 0; j >= 0; --j, ++x) {
-            rotacionada.pixel[i][j][0] = img.pixel[x][y][0];
-            rotacionada.pixel[i][j][1] = img.pixel[x][y][1];
-            rotacionada.pixel[i][j][2] = img.pixel[x][y][2];
-        }
-    }
-
-    return rotacionada;
-}
-
+int max(int a, int b);
+int pixel_igual(Pixel p1, Pixel p2);
+Image escala_de_cinza(Image img);
+void blur(unsigned int h, unsigned short int pixel[512][512][3], int T, unsigned int w);
+Image rotacionar90direita(Image img);
 void inverter_cores(unsigned short int pixel[512][512][3],
-                    unsigned int w, unsigned int h) {
-    for (unsigned int i = 0; i < h; ++i) {
-        for (unsigned int j = 0; j < w; ++j) {
-            pixel[i][j][0] = 255 - pixel[i][j][0];
-            pixel[i][j][1] = 255 - pixel[i][j][1];
-            pixel[i][j][2] = 255 - pixel[i][j][2];
-        }
-    }
-}
+                    unsigned int w, unsigned int h);
+Image cortar_imagem(Image img, int x, int y, int w, int h);
 
-Image cortar_imagem(Image img, int x, int y, int w, int h) {
-    Image cortada;
-
-    cortada.w = w;
-    cortada.h = h;
-
-    for(int i = 0; i < h; ++i) {
-        for(int j = 0; j < w; ++j) {
-            cortada.pixel[i][j][0] = img.pixel[i + y][j + x][0];
-            cortada.pixel[i][j][1] = img.pixel[i + y][j + x][1];
-            cortada.pixel[i][j][2] = img.pixel[i + y][j + x][2];
-        }
-    }
-
-    return cortada;
-}
 
 
 int main() {
@@ -264,4 +164,117 @@ int main() {
         printf("\n");
     }
     return 0;
+}
+
+
+//-------------------------------------------------------------------
+
+
+int max(int a, int b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
+int pixel_igual(Pixel p1, Pixel p2) {
+    if (p1.r == p2.r &&
+        p1.g == p2.g &&
+        p1.b == p2.b)
+        return 1;
+    return 0;
+}
+
+
+Image escala_de_cinza(Image img) {
+    /*for (unsigned int i = 0; i < img.h; ++i) {
+        for (unsigned int j = 0; j < img.w; ++j) {
+            print("%u", img.pixel[i][j][0] + img.pixel[i][j][1] + img.pixel[i][j][2]);
+        }
+    }*/
+
+    for (unsigned int i = 0; i < img.h; ++i) {
+        for (unsigned int j = 0; j < img.w; ++j) {
+            int media = img.pixel[i][j][0] +
+                        img.pixel[i][j][1] +
+                        img.pixel[i][j][2];
+            media /= 3;
+            img.pixel[i][j][0] = media;
+            img.pixel[i][j][1] = media;
+            img.pixel[i][j][2] = media;
+        }
+    }
+
+    return img;
+}
+
+void blur(unsigned int h, unsigned short int pixel[512][512][3], int T, unsigned int w) {
+    for (unsigned int i = 0; i < h; ++i) {
+        for (unsigned int j = 0; j < w; ++j) {
+            Pixel media = {0, 0, 0};
+
+            int menor_h = (h - 1 > i + T/2) ? i + T/2 : h - 1;
+            int min_w = (w - 1 > j + T/2) ? j + T/2 : w - 1;
+            for(int x = (0 > i - T/2 ? 0 : i - T/2); x <= menor_h; ++x) {
+                for(int y = (0 > j - T/2 ? 0 : j - T/2); y <= min_w; ++y) {
+                    media.r += pixel[x][y][0];
+                    media.g += pixel[x][y][1];
+                    media.b += pixel[x][y][2];
+                }
+            }
+
+            // printf("%u", media.r)
+            media.r /= T * T;
+            media.g /= T * T;
+            media.b /= T * T;
+
+            pixel[i][j][0] = media.r;
+            pixel[i][j][1] = media.g;
+            pixel[i][j][2] = media.b;
+        }
+    }
+}
+
+Image rotacionar90direita(Image img) {
+    Image rotacionada;
+
+    rotacionada.w = img.h;
+    rotacionada.h = img.w;
+
+    for (unsigned int i = 0, y = 0; i < rotacionada.h; ++i, ++y) {
+        for (int j = rotacionada.w - 1, x = 0; j >= 0; --j, ++x) {
+            rotacionada.pixel[i][j][0] = img.pixel[x][y][0];
+            rotacionada.pixel[i][j][1] = img.pixel[x][y][1];
+            rotacionada.pixel[i][j][2] = img.pixel[x][y][2];
+        }
+    }
+
+    return rotacionada;
+}
+
+void inverter_cores(unsigned short int pixel[512][512][3],
+                    unsigned int w, unsigned int h) {
+    for (unsigned int i = 0; i < h; ++i) {
+        for (unsigned int j = 0; j < w; ++j) {
+            pixel[i][j][0] = 255 - pixel[i][j][0];
+            pixel[i][j][1] = 255 - pixel[i][j][1];
+            pixel[i][j][2] = 255 - pixel[i][j][2];
+        }
+    }
+}
+
+Image cortar_imagem(Image img, int x, int y, int w, int h) {
+    Image cortada;
+
+    cortada.w = w;
+    cortada.h = h;
+
+    for(int i = 0; i < h; ++i) {
+        for(int j = 0; j < w; ++j) {
+            cortada.pixel[i][j][0] = img.pixel[i + y][j + x][0];
+            cortada.pixel[i][j][1] = img.pixel[i + y][j + x][1];
+            cortada.pixel[i][j][2] = img.pixel[i + y][j + x][2];
+        }
+    }
+
+    return cortada;
 }
---
+++ tests.sh
@@ -0,0 +1,30 @@
+# Compilando o código
+gcc main.c -o photopobre
+# Testando o método inverter cores
+cat lena/lena.ppm lena/inverter_cores.txt | ./photopobre > saida.ppm
+echo "Inverter cores:"
+diff lena/inverter_cores.ppm saida.ppm
+# Testando o método blur 15
+#cat lena/lena.ppm lena/blur_15.txt | ./photopobre > saida.ppm
+#echo "Blur 15:"
+#diff lena/blur_15.ppm saida.ppm
+
+cat lena/lena.ppm lena/cortar.txt | ./photopobre > saida.ppm
+echo "Cortar:"
+diff lena/cortar.ppm saida.ppm
+
+cat lena/lena.ppm lena/escala_cinza.txt | ./photopobre > saida.ppm
+echo "Escala cinza:"
+diff lena/escala_cinza.ppm saida.ppm
+
+cat lena/lena.ppm lena/espelhamento_vertical.txt | ./photopobre > saida.ppm
+echo "Espelhamento vertical:"
+diff lena/espelhamento_vertical.ppm saida.ppm
+
+cat lena/lena.ppm lena/rotacao_90.txt | ./photopobre > saida.ppm
+echo "Rotacao 90:"
+diff lena/rotacao_90.ppm saida.ppm
+
+cat lena/lena.ppm lena/sepia.txt | ./photopobre > saida.ppm
+echo "Sepia:"
+diff lena/sepia.ppm saida.ppm
